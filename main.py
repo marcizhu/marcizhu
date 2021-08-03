cfcbf1246269770105853b8659627b0b9e3afee8
marcizhu <marc.izquierdo.hu@gmail.com>
2021-08-03 11:00:40 +0200

    Update chess bot
    Fix #535

--- main.py
+++ main.py
@@ -2,205 +2,224 @@ import re
 import os
 import os.path
 import sys
-import chess
-import chess.pgn
 import ast
-
-from github import Github
 from enum import Enum
 from datetime import datetime
 
-import src.tweaks as tweaks
-import src.markdown as markdown
+import chess
+import chess.pgn
+import yaml
+from github import Github
 
-# TODO:
-# - Use an image instead of a raw link to start new games
+import src.markdown as markdown
+import src.selftest as selftest
+import src.mockGithub as mockGithub
+
+# TODO: Use an image instead of a raw link to start new games
 
 class Action(Enum):
-	UNKNOWN = 0
-	MOVE = 1
-	NEW_GAME = 2
+    UNKNOWN = 0
+    MOVE = 1
+    NEW_GAME = 2
 
 
 def update_top_moves(user):
-	with open("data/top_moves.txt", 'r') as file:
-		contents = file.read()
-		dictionary = ast.literal_eval(contents)
+    """Adds the given user to the top moves file"""
+    with open('data/top_moves.txt', 'r') as file:
+        contents = file.read()
+        dictionary = ast.literal_eval(contents)
 
-	if user not in dictionary:
-		dictionary[user] = 1 # First move
-	else:
-		dictionary[user] += 1
+    if user not in dictionary:
+        dictionary[user] = 1 # First move
+    else:
+        dictionary[user] += 1
 
-	with open("data/top_moves.txt", 'w') as file:
-		file.write(str(dictionary))
+    with open('data/top_moves.txt', 'w') as file:
+        file.write(str(dictionary))
 
 
 def update_last_moves(line):
-	with open("data/last_moves.txt", 'r+') as f:
-		content = f.read()
-		f.seek(0, 0)
-		f.write(line.rstrip('\r\n') + '\n' + content)
+    """Adds the given line to the last moves file"""
+    with open('data/last_moves.txt', 'r+') as last_moves:
+        content = last_moves.read()
+        last_moves.seek(0, 0)
+        last_moves.write(line.rstrip('\r\n') + '\n' + content)
 
 
-def replaceTextBetween(originalText, delimeterA, delimterB, replacementText):
-	if originalText.find(delimeterA) == -1 or originalText.find(delimterB) == -1:
-		return originalText
+def replace_text_between(original_text, marker, replacement_text):
+    """Replace text between `marker['begin']` and `marker['end']` with `replacement`"""
+    delimiter_a = marker['begin']
+    delimiter_b = marker['end']
 
-	leadingText = originalText.split(delimeterA)[0]
-	trailingText = originalText.split(delimterB)[1]
+    if original_text.find(delimiter_a) == -1 or original_text.find(delimiter_b) == -1:
+        return original_text
 
-	return leadingText + delimeterA + replacementText + delimterB + trailingText
+    leading_text = original_text.split(delimiter_a)[0]
+    trailing_text = original_text.split(delimiter_b)[1]
+
+    return leading_text + delimiter_a + replacement_text + delimiter_b + trailing_text
 
 
 def parse_issue(title):
-	if title.lower() == "chess: start new game":
-		return (Action.NEW_GAME, None)
-	elif "chess: move" in title.lower():
-		matchObj = re.match('Chess: Move ([A-H][1-8]) to ([A-H][1-8])', title, re.I)
-		
-		source = matchObj.group(1)
-		dest   = matchObj.group(2)
-		return (Action.MOVE, (source + dest).lower())
+    """Parse issue title and return a tuple with (action, <move>)"""
+    if title.lower() == 'chess: start new game':
+        return (Action.NEW_GAME, None)
 
-	return (Action.UNKNOWN, None)
+    if 'chess: move' in title.lower():
+        match_obj = re.match('Chess: Move ([A-H][1-8]) to ([A-H][1-8])', title, re.I)
 
+        source = match_obj.group(1)
+        dest   = match_obj.group(2)
+        return (Action.MOVE, (source + dest).lower())
 
-def main():
-	g = Github(os.environ["GITHUB_TOKEN"])
-	repo = g.get_repo(os.environ["GITHUB_REPOSITORY"])
-	issue = repo.get_issue(number=int(os.environ["ISSUE_NUMBER"]))
-
-	issue_title  = issue.title
-	issue_author = "@" + issue.user.login
-
-	action = parse_issue(issue_title)
-	gameboard = chess.Board()
-
-	if action[0] == Action.NEW_GAME:
-		if os.path.exists("games/current.pgn") and issue_author != "@" + os.environ["REPOSITORY_OWNER"]:
-			issue.create_comment(tweaks.COMMENT_INVALID_NEW_GAME.format(author=issue_author))
-			issue.edit(state='closed')
-			sys.exit("ERROR: A current game is in progress. Only the repo owner can start a new issue")
-
-		print("Start new game")
-		issue.create_comment(tweaks.COMMENT_SUCCESSFUL_NEW_GAME.format(author=issue_author))
-		issue.edit(state='closed')
-
-		with open("data/last_moves.txt", 'w') as f:
-			f.write("Start game: " + issue_author)
-
-		# Create new game
-		game = chess.pgn.Game()
-		game.headers["Event"] = os.environ["REPOSITORY_OWNER"] + "'s Online Open Chess Tournament"
-		game.headers["Site"] = "https://github.com/" + os.environ["GITHUB_REPOSITORY"]
-		game.headers["Date"] = datetime.now().strftime("%Y.%m.%d")
-		game.headers["Round"] = "1"
-
-	elif action[0] == Action.MOVE:
-		if not os.path.exists("games/current.pgn"):
-			sys.exit("ERROR: There is no game in progress! Start a new game first")
-
-		# Load game from "games/current.pgn"
-		pgn_file = open("games/current.pgn")
-		game = chess.pgn.read_game(pgn_file)
-		gameboard = game.board()
-
-		with open('data/last_moves.txt') as moves:
-			line = moves.readline()
-			last_player = line.split(':')[1].strip()
-			last_move   = line.split(':')[0].strip()
-
-		for move in game.mainline_moves():
-			gameboard.push(move)
-
-		# Try to move with promotion to queen
-		if chess.Move.from_uci(action[1] + "q") in gameboard.legal_moves:
-			action = (action[0], action[1] + "q")
-
-		print("Perform move " + action[1])
-
-		move = chess.Move.from_uci(action[1])
-
-		# Check if player is moving twice in a row
-		if last_player == issue_author and "Start game" not in last_move:
-			issue.create_comment("Sorry, you can't move twice in a row! You can ask someone to play the next turn :D")
-			issue.edit(state='closed', labels=["Invalid"])
-			sys.exit("ERROR: Two moves in a row!")
-
-		# Check if move is valid
-		if not move in gameboard.legal_moves:
-			issue.create_comment(tweaks.COMMENT_INVALID_MOVE.format(author=issue_author, move=action[1]))
-			issue.edit(state='closed', labels=["Invalid"])
-			sys.exit("ERROR: Move is invalid!")
-
-		# Check if board is valid
-		if not gameboard.is_valid():
-			issue.create_comment(tweaks.COMMENT_INVALID_BOARD.format(author=issue_author))
-			issue.edit(state='closed', labels=["Invalid"])
-			sys.exit("ERROR: Board is invalid!")
-
-		issue_labels = ["⚔️ Capture!"] if gameboard.is_capture(move) else []
-		issue_labels += ["White" if gameboard.turn == chess.WHITE else "Black"]
-		
-		issue.create_comment(tweaks.COMMENT_SUCCESSFUL_MOVE.format(author=issue_author, move=action[1]))
-		issue.edit(state='closed', labels=issue_labels)
-
-		update_last_moves(action[1] + ": " + issue_author)
-		update_top_moves(issue_author)
-
-		# Perform move
-		gameboard.push(move)
-		game.end().add_main_variation(move, comment=issue_author)
-		game.headers["Result"] = gameboard.result()
-
-	elif action[0] == Action.UNKNOWN:
-		issue.create_comment(tweaks.COMMENT_UNKNOWN_COMMAND.format(author=issue_author))
-		issue.edit(state='closed', labels=["Invalid"])
-		sys.exit("ERROR: Unknown action")
-
-	# Save game to "games/current.pgn"
-	print(game, file=open("games/current.pgn", "w"), end="\n\n")
-
-	turn = "white" if gameboard.turn == chess.WHITE else "black"
-	moves = markdown.generate_moves_list(gameboard)
-	board = markdown.board_to_markdown(gameboard)
-	lasts = markdown.generate_last_moves()
-	top   = markdown.generate_top_moves()
-
-	# If it is a game over, archive current game
-	if gameboard.is_game_over():
-		winner = gameboard.result()
-		win_msg= "It's a draw"
-
-		if winner == "1-0":
-			win_msg = "White wins"
-		elif winner == "0-1":
-			win_msg = "Black wins"
-
-		with open("data/last_moves.txt", "r") as f:
-			lines = f.readlines()
-			pattern = re.compile('.*: (@[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38})', flags=re.I)
-			player_list = [re.match(pattern, line).group(1) for line in lines]
-			players = ", ".join(set(player_list))
-
-		issue.add_to_labels("👑 Winner!")
-		issue.create_comment(tweaks.COMMENT_GAME_OVER.format(outcome=win_msg, players=players, num_moves=len(lines), num_players=len(set(player_list))))
-		os.rename("games/current.pgn", datetime.now().strftime("games/game-%Y%m%d-%H%M%S.pgn"))
-		os.remove("data/last_moves.txt")
-
-	with open("README.md", "r") as file:
-		readme = file.read()
-		readme = replaceTextBetween(readme, tweaks.BOARD_BEGIN_MARKER, tweaks.BOARD_END_MARKER, "{chess_board}")
-		readme = replaceTextBetween(readme, tweaks.MOVES_BEGIN_MARKER, tweaks.MOVES_END_MARKER, "{moves_list}")
-		readme = replaceTextBetween(readme, tweaks.TURN_BEGIN_MARKER,  tweaks.TURN_END_MARKER,  "{turn}")
-		readme = replaceTextBetween(readme, tweaks.LAST_MOVES_BEGIN_MARKER, tweaks.LAST_MOVES_END_MARKER, "{last_moves}")
-		readme = replaceTextBetween(readme, tweaks.TOP_MOVERS_BEGIN_MARKER, tweaks.TOP_MOVERS_END_MARKER, "{top_moves}")
-
-	with open("README.md", "w") as file:
-		# Write new board & list of movements
-		file.write(readme.format(chess_board=board, moves_list=moves, turn=turn, last_moves=lasts, top_moves=top))
+    return (Action.UNKNOWN, None)
 
 
-if __name__ == "__main__":
-	main()
+def main(issue, issue_author, repo_owner):
+    action = parse_issue(issue.title)
+    gameboard = chess.Board()
+
+    with open('data/settings.yaml', 'r') as settings_file:
+        settings = yaml.load(settings_file, Loader=yaml.FullLoader)
+
+    if action[0] == Action.NEW_GAME:
+        if os.path.exists('games/current.pgn') and issue_author != repo_owner:
+            issue.create_comment(settings['comments']['invalid_new_game'].format(author=issue_author))
+            issue.edit(state='closed')
+            return False, 'ERROR: A current game is in progress. Only the repo owner can start a new game'
+
+        issue.create_comment(settings['comments']['successful_new_game'].format(author=issue_author))
+        issue.edit(state='closed')
+
+        with open('data/last_moves.txt', 'w') as last_moves:
+            last_moves.write('Start game: ' + issue_author)
+
+        # Create new game
+        game = chess.pgn.Game()
+        game.headers['Event'] = repo_owner + '\'s Online Open Chess Tournament'
+        game.headers['Site'] = 'https://github.com/' + os.environ['GITHUB_REPOSITORY']
+        game.headers['Date'] = datetime.now().strftime('%Y.%m.%d')
+        game.headers['Round'] = '1'
+
+    elif action[0] == Action.MOVE:
+        if not os.path.exists('games/current.pgn'):
+            return False, 'ERROR: There is no game in progress! Start a new game first'
+
+        # Load game from "games/current.pgn"
+        with open('games/current.pgn') as pgn_file:
+            game = chess.pgn.read_game(pgn_file)
+            gameboard = game.board()
+
+        with open('data/last_moves.txt') as moves:
+            line = moves.readline()
+            last_player = line.split(':')[1].strip()
+            last_move   = line.split(':')[0].strip()
+
+        for move in game.mainline_moves():
+            gameboard.push(move)
+
+        # Try to move with promotion to queen
+        if chess.Move.from_uci(action[1] + 'q') in gameboard.legal_moves:
+            action = (action[0], action[1] + 'q')
+
+        move = chess.Move.from_uci(action[1])
+
+        # Check if player is moving twice in a row
+        if last_player == issue_author and 'Start game' not in last_move:
+            issue.create_comment(settings['comments']['consecutive_moves'].format(author=issue_author))
+            issue.edit(state='closed', labels=['Invalid'])
+            return False, 'ERROR: Two moves in a row!'
+
+        # Check if move is valid
+        if move not in gameboard.legal_moves:
+            issue.create_comment(settings['comments']['invalid_move'].format(author=issue_author, move=action[1]))
+            issue.edit(state='closed', labels=['Invalid'])
+            return False, 'ERROR: Move is invalid!'
+
+        # Check if board is valid
+        if not gameboard.is_valid():
+            issue.create_comment(settings['comments']['invalid_board'].format(author=issue_author))
+            issue.edit(state='closed', labels=['Invalid'])
+            return False, 'ERROR: Board is invalid!'
+
+        issue_labels = ['⚔️ Capture!'] if gameboard.is_capture(move) else []
+        issue_labels += ['White' if gameboard.turn == chess.WHITE else 'Black']
+
+        issue.create_comment(settings['comments']['successful_move'].format(author=issue_author, move=action[1]))
+        issue.edit(state='closed', labels=issue_labels)
+
+        update_last_moves(action[1] + ': ' + issue_author)
+        update_top_moves(issue_author)
+
+        # Perform move
+        gameboard.push(move)
+        game.end().add_main_variation(move, comment=issue_author)
+        game.headers['Result'] = gameboard.result()
+
+    elif action[0] == Action.UNKNOWN:
+        issue.create_comment(settings['comments']['unknown_command'].format(author=issue_author))
+        issue.edit(state='closed', labels=['Invalid'])
+        return False, 'ERROR: Unknown action'
+
+    # Save game to "games/current.pgn"
+    print(game, file=open('games/current.pgn', 'w'), end='\n\n')
+
+    last_moves = markdown.generate_last_moves()
+
+    # If it is a game over, archive current game
+    if gameboard.is_game_over():
+        win_msg = {
+            '1/2-1/2': 'It\'s a draw',
+            '1-0': 'White wins',
+            '0-1': 'Black wins'
+        }
+
+        with open('data/last_moves.txt', 'r') as last_moves:
+            lines = last_moves.readlines()
+            pattern = re.compile('.*: (@[a-z\\d](?:[a-z\\d]|-(?=[a-z\\d])){0,38})', flags=re.I)
+            player_list = { re.match(pattern, line).group(1) for line in lines }
+
+        issue.add_to_labels('👑 Winner!')
+        issue.create_comment(settings['comments']['game_over'].format(
+            outcome=win_msg.get(gameboard.result(), 'UNKNOWN'),
+            players=', '.join(player_list),
+            num_moves=len(lines)-1,
+            num_players=len(player_list)))
+
+        os.rename('games/current.pgn', datetime.now().strftime('games/game-%Y%m%d-%H%M%S.pgn'))
+        os.remove('data/last_moves.txt')
+
+    with open('README.md', 'r') as file:
+        readme = file.read()
+        readme = replace_text_between(readme, settings['markers']['board'], '{chess_board}')
+        readme = replace_text_between(readme, settings['markers']['moves'], '{moves_list}')
+        readme = replace_text_between(readme, settings['markers']['turn'],  '{turn}')
+        readme = replace_text_between(readme, settings['markers']['last_moves'], '{last_moves}')
+        readme = replace_text_between(readme, settings['markers']['top_moves'], '{top_moves}')
+
+    with open('README.md', 'w') as file:
+        # Write new board & list of movements
+        file.write(readme.format(
+            chess_board=markdown.board_to_markdown(gameboard),
+            moves_list=markdown.generate_moves_list(gameboard),
+            turn=('white' if gameboard.turn == chess.WHITE else 'black'),
+            last_moves=last_moves,
+            top_moves=markdown.generate_top_moves()))
+
+    return True, ''
+
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 2 and sys.argv[1] == '--self-test':
+        selftest.run(main)
+        sys.exit(0)
+    else:
+        repo = Github(os.environ['GITHUB_TOKEN']).get_repo(os.environ['GITHUB_REPOSITORY'])
+        issue = repo.get_issue(number=int(os.environ['ISSUE_NUMBER']))
+        issue_author = '@' + issue.user.login
+        repo_owner = '@' + os.environ['REPOSITORY_OWNER']
+
+    ret, reason = main(issue, issue_author, repo_owner)
+
+    if ret == False:
+        sys.exit(reason)
--- src/markdown.py
+++ src/markdown.py
@@ -1,148 +1,168 @@
+from collections import defaultdict
+from urllib.parse import urlencode
+import os
 import re
 import ast
-import chess
-import src.tweaks as tweaks
 
-from collections import defaultdict
+import chess
+import yaml
+
+with open('data/settings.yaml', 'r') as settings_file:
+    settings = yaml.load(settings_file, Loader=yaml.FullLoader)
 
 
 def create_link(text, link):
-	return "[" + text + "](" + link + ")"
+    return "[" + text + "](" + link + ")"
 
 
-def create_issue_link(source, dest_list):	
-	ret = []
+def create_issue_link(source, dest_list):
+    ret = []
 
-	for dest in sorted(dest_list):
-		ret.append(create_link(dest, tweaks.GITHUB_MOVE_ISSUE_LINK.format(source=source, dest=dest)))
+    issue_link = settings['issues']['link'].format(
+        repo=os.environ["GITHUB_REPOSITORY"],
+        params=urlencode(settings['issues']['move'], safe="{}"))
 
-	return ", ".join(ret)
+    for dest in sorted(dest_list):
+        ret.append(create_link(dest, issue_link.format(source=source, dest=dest)))
+
+    return ", ".join(ret)
 
 
 def generate_top_moves():
-	with open("data/top_moves.txt", 'r') as file:
-		contents = file.read()
-		dictionary = ast.literal_eval(contents)
+    with open("data/top_moves.txt", 'r') as file:
+        contents = file.read()
+        dictionary = ast.literal_eval(contents)
 
-	markdown = "\n"
-	markdown += "| Total moves |  User  |\n"
-	markdown += "| :---------: | :----- |\n"
+    markdown = "\n"
+    markdown += "| Total moves |  User  |\n"
+    markdown += "| :---------: | :----- |\n"
 
-	counter = 0
-	for key,val in sorted(dictionary.items(), key=lambda x: x[1], reverse=True):
-		if counter >= tweaks.MAX_TOP_MOVERS: break
-		counter += 1
-		markdown += "| " + str(val) + " | " + create_link(key, "https://github.com/" + key[1:]) + " |\n"
+    counter = 0
+    for key,val in sorted(dictionary.items(), key=lambda x: x[1], reverse=True):
+        if counter >= settings['misc']['max_top_moves']:
+            break
 
-	return markdown + "\n"
+        counter += 1
+        markdown += "| " + str(val) + " | " + create_link(key, "https://github.com/" + key[1:]) + " |\n"
+
+    return markdown + "\n"
 
 
 def generate_last_moves():
-	markdown = "\n"
-	markdown += "| Move | Author |\n"
-	markdown += "| :--: | :----- |\n"
+    markdown = "\n"
+    markdown += "| Move | Author |\n"
+    markdown += "| :--: | :----- |\n"
 
-	counter = 0
+    counter = 0
 
-	with open("data/last_moves.txt", 'r') as file:
-		for line in file.readlines():
-			parts = line.rstrip().split(':')
-			if not ":" in line: continue
-			if counter >= tweaks.MAX_LAST_MOVERS: break
-			counter += 1
+    with open("data/last_moves.txt", 'r') as file:
+        for line in file.readlines():
+            parts = line.rstrip().split(':')
 
-			matchObj = re.search('([A-H][1-8])([A-H][1-8])', line, re.I)
-			if matchObj != None:
-				source = matchObj.group(1).upper()
-				dest   = matchObj.group(2).upper()
+            if not ":" in line:
+                continue
 
-				markdown += "| `" + source + "` to `" + dest + "` | " + create_link(parts[1], "https://github.com/" + parts[1].lstrip()[1:]) + " |\n"
-			else:
-				markdown += "| `" + parts[0] + "` | " + create_link(parts[1], "https://github.com/" + parts[1].lstrip()[1:]) + " |\n"
+            if counter >= settings['misc']['max_last_moves']:
+                break
 
-	return markdown + "\n"
+            counter += 1
+
+            match_obj = re.search('([A-H][1-8])([A-H][1-8])', line, re.I)
+            if match_obj is not None:
+                source = match_obj.group(1).upper()
+                dest   = match_obj.group(2).upper()
+
+                markdown += "| `" + source + "` to `" + dest + "` | " + create_link(parts[1], "https://github.com/" + parts[1].lstrip()[1:]) + " |\n"
+            else:
+                markdown += "| `" + parts[0] + "` | " + create_link(parts[1], "https://github.com/" + parts[1].lstrip()[1:]) + " |\n"
+
+    return markdown + "\n"
 
 
 def generate_moves_list(board):
-	# Create dictionary and fill it
-	moves = list(board.legal_moves)
-	moves_dict = defaultdict(list)
+    # Create dictionary and fill it
+    moves = list(board.legal_moves)
+    moves_dict = defaultdict(set)
 
-	for move in moves:
-		source = chess.SQUARE_NAMES[move.from_square].upper()
-		dest   = chess.SQUARE_NAMES[move.to_square].upper()
+    for move in moves:
+        source = chess.SQUARE_NAMES[move.from_square].upper()
+        dest   = chess.SQUARE_NAMES[move.to_square].upper()
 
-		moves_dict[source].append(dest)
+        moves_dict[source].add(dest)
 
-	# Write everything in Markdown format
-	markdown = ""
+    # Write everything in Markdown format
+    markdown = ""
+    issue_link = settings['issues']['link'].format(
+        repo=os.environ["GITHUB_REPOSITORY"],
+        params=urlencode(settings['issues']['new_game']))
 
-	if board.is_game_over():
-		return "**GAME IS OVER!** " + create_link("Click here", tweaks.GITHUB_NEW_GAME_ISSUE_LINK) + " to start a new game :D\n"
-	elif board.is_check():
-		markdown += "**CHECK!** Choose your move wisely!\n"
+    if board.is_game_over():
+        return "**GAME IS OVER!** " + create_link("Click here", issue_link) + " to start a new game :D\n"
 
-	markdown += "|  FROM  | TO (Just click a link!) |\n"
-	markdown += "| :----: | :---------------------- |\n"
+    if board.is_check():
+        markdown += "**CHECK!** Choose your move wisely!\n"
 
-	for source,dest in sorted(moves_dict.items()):
-		markdown += "| **" + source + "** | " + create_issue_link(source, dest) + " |\n"
+    markdown += "|  FROM  | TO (Just click a link!) |\n"
+    markdown += "| :----: | :---------------------- |\n"
 
-	return markdown
+    for source,dest in sorted(moves_dict.items()):
+        markdown += "| **" + source + "** | " + create_issue_link(source, dest) + " |\n"
+
+    return markdown
 
 
 def board_to_list(board):
-	board_list = []
+    board_list = []
 
-	for line in board.split('\n'):
-		sublist = []
-		for item in line.split(' '):
-			sublist.append(item)
+    for line in board.split('\n'):
+        sublist = []
+        for item in line.split(' '):
+            sublist.append(item)
 
-		board_list.append(sublist)
+        board_list.append(sublist)
 
-	return board_list
+    return board_list
 
 
 def get_image_link(piece):
-	switcher = {
-		"r": "img/black/rook.png",
-		"n": "img/black/knight.png",
-		"b": "img/black/bishop.png",
-		"q": "img/black/queen.png",
-		"k": "img/black/king.png",
-		"p": "img/black/pawn.png",
+    switcher = {
+        "r": "img/black/rook.png",
+        "n": "img/black/knight.png",
+        "b": "img/black/bishop.png",
+        "q": "img/black/queen.png",
+        "k": "img/black/king.png",
+        "p": "img/black/pawn.png",
 
-		"R": "img/white/rook.png",
-		"N": "img/white/knight.png",
-		"B": "img/white/bishop.png",
-		"Q": "img/white/queen.png",
-		"K": "img/white/king.png",
-		"P": "img/white/pawn.png",
+        "R": "img/white/rook.png",
+        "N": "img/white/knight.png",
+        "B": "img/white/bishop.png",
+        "Q": "img/white/queen.png",
+        "K": "img/white/king.png",
+        "P": "img/white/pawn.png",
 
-		".": "img/blank.png"
-	}
+        ".": "img/blank.png"
+    }
 
-	return switcher.get(piece, "???")
+    return switcher.get(piece, "???")
 
 
 def board_to_markdown(board):
-	l = board_to_list(str(board))
-	markdown = ""
+    board_list = board_to_list(str(board))
+    markdown = ""
 
-	# Write header in Markdown format
-	markdown += "|   | A | B | C | D | E | F | G | H |   |\n"
-	markdown += "|---|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|\n"
+    # Write header in Markdown format
+    markdown += "|   | A | B | C | D | E | F | G | H |   |\n"
+    markdown += "|---|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|\n"
 
-	# Write board
-	for row in range(1, 9):
-		markdown += "| **" + str(9 - row) + "** | "
-		for elem in l[row - 1]:
-			markdown += "<img src=\"{}\" width=50px> | ".format(get_image_link(elem))
+    # Write board
+    for row in range(1, 9):
+        markdown += "| **" + str(9 - row) + "** | "
+        for elem in board_list[row - 1]:
+            markdown += "<img src=\"{}\" width=50px> | ".format(get_image_link(elem))
 
-		markdown += "**" + str(9 - row) + "** |\n"
+        markdown += "**" + str(9 - row) + "** |\n"
 
-	# Write footer in Markdown format
-	markdown += "|   | **A** | **B** | **C** | **D** | **E** | **F** | **G** | **H** |   |\n"
+    # Write footer in Markdown format
+    markdown += "|   | **A** | **B** | **C** | **D** | **E** | **F** | **G** | **H** |   |\n"
 
-	return markdown
+    return markdown
